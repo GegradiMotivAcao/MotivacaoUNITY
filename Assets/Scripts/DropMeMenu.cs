c1d9afa61904d101e3df516807ec0de538f0b052
BrunoIost <bsiost@inf.ufpel.edu.br>
2021-10-19 20:30:09 -0300

    Exclusao funcionando

--- Assets/Scripts/DropMeMenu.cs
+++ Assets/Scripts/DropMeMenu.cs
@@ -18,9 +18,10 @@ public class DropMeMenu : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPo
 
 	void Start ()
 	{
+		button_apagar = GameObject.Find("btn_apagar");
 		toggle_apagar = button_apagar.GetComponent<Toggle> ();
-		AddPhysics2DRaycaster();
-		//adicionar gameobject.find  apagar
+		// AddPhysics2DRaycaster();		
+		
 	}
 
 	
@@ -36,9 +37,9 @@ public class DropMeMenu : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPo
 		if (receivingImage == null)
 			return;
 
-		var originalObj = data.pointerDrag;											   // IF ANTI-BAGUNÇA >:(
-		if ( (originalObj != respectiveImage[0]) && (originalObj != respectiveImage[1]) ) //Aqui ocorre a verificação de que imagem vai em qual gamespot
-			return;    																	 // Se desativar, pode dropar em qualquer local
+		// var originalObj = data.pointerDrag;											   // IF ANTI-BAGUNÇA >:(
+		// if ( (originalObj != respectiveImage[0]) && (originalObj != respectiveImage[1]) ) //Aqui ocorre a verificação de que imagem vai em qual gamespot
+		// 	return;    																	 // Se desativar, pode dropar em qualquer local
 		
 
 		//PARA PEGAR SO AS IMGENS NO DRAG
@@ -63,9 +64,9 @@ public class DropMeMenu : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPo
 
 		//Debug.Break();
 
-		var originalObj = data.pointerDrag; 												// IF ANTI-BAGUNÇA part2 - O império contra ataca
-		if ( (originalObj != respectiveImage[0]) && (originalObj != respectiveImage[1]) )   // Aqui ele verifica se mostra o highlight apenas para a imagem "certa"
-			return;																	// Desativando conseguimos ver o highlight em qualquer spot pra qqlr imagem
+		// var originalObj = data.pointerDrag; 												// IF ANTI-BAGUNÇA part2 - O império contra ataca
+		// if ( (originalObj != respectiveImage[0]) && (originalObj != respectiveImage[1]) )   // Aqui ele verifica se mostra o highlight apenas para a imagem "certa"
+		// 	return;																	// Desativando conseguimos ver o highlight em qualquer spot pra qqlr imagem
 	
 		Sprite dropSprite = GetDropSprite (data);
 		if (dropSprite != null) {
